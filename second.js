46efddd8873b9a189b3f41af160a5282805b2934
Maedot Amha <maedotamha@gmail.com>
2023-11-29 11:02:47 +0300

    commit

--- second.js
+++ second.js
@@ -1,6 +1,6 @@
 // === checks the data type while == doesnt
 
-const mass = prompt ("what is your mass ")
-const height = prompt ("what is your height ")
-const BMI = mass / (height *height)
-console.log(BMI)
+const mass = prompt ("what is your mass ");
+const height = prompt ("what is your height ");
+const BMI = mass / (height *height);
+console.log(BMI);
